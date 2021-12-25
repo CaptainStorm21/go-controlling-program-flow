febb51479c73178a9cfd575930be7cd248038aed
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-12-24 21:38:36 -0800

    pacnic function

---
+++ panic-functions.go
@@ -0,0 +1,14 @@
+package main
+
+import {
+	"fmt"
+}
+
+func main() {
+	fmt.println("starting a webserver")
+
+	//do important things
+	panic("Something is bad going on ")
+
+	fmt.println("Web server started")
+}
